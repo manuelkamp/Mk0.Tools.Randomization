ba8bdb7a04952f0f2eb92e788a17d3464b61fe65
Manuel Kamper <manuelk@outlook.com>
2019-03-22 23:22:24 +0100

    Initial

---
+++ Mk0.Tools.Randomization/Randomize.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+
+namespace Mk0.Tools.Randomization
+{
+    public class Randomize
+    {
+        /// <summary>
+        /// Generiert einen zufälligen String aus Zahlen und Buchstaben mit optionalem Endzeichen
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="endzeichen"></param>
+        /// <returns>Zufallsstring</returns>
+        public static string NumbersAndDigits(int length, string beginnzeichen = "", string endzeichen = "")
+        {
+            string ret = string.Empty;
+            StringBuilder SB = new StringBuilder();
+            string Content = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw";
+            Random rnd = new Random();
+            if (beginnzeichen != "")
+            {
+                SB.Append(beginnzeichen);
+            }
+            for (int i = 0; i < length; i++)
+                SB.Append(Content[rnd.Next(Content.Length)]);
+            if (endzeichen != "")
+            {
+                SB.Append(endzeichen);
+            }
+            return SB.ToString();
+        }
+
+        /// <summary>
+        /// Generiert einen zufälligen String Buchstaben mit optionalem Endzeichen
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="endzeichen"></param>
+        /// <returns>Zufallsstring</returns>
+        public static string Digits(int length, string beginnzeichen = "", string endzeichen = "")
+        {
+            string ret = string.Empty;
+            StringBuilder SB = new StringBuilder();
+            string Content = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw";
+            Random rnd = new Random();
+            if (beginnzeichen != "")
+            {
+                SB.Append(beginnzeichen);
+            }
+            for (int i = 0; i < length; i++)
+                SB.Append(Content[rnd.Next(Content.Length)]);
+            if (endzeichen != "")
+            {
+                SB.Append(endzeichen);
+            }
+            return SB.ToString();
+        }
+
+        /// <summary>
+        /// Generiert einen zufälligen String aus Zahlen mit optionalem Endzeichen
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="endzeichen"></param>
+        /// <returns>Zufallsstring</returns>
+        public static string Numbers(int length, string beginnzeichen = "", string endzeichen = "")
+        {
+            string ret = string.Empty;
+            StringBuilder SB = new StringBuilder();
+            string Content = "1234567890";
+            Random rnd = new Random();
+            if (beginnzeichen != "")
+            {
+                SB.Append(beginnzeichen);
+            }
+            for (int i = 0; i < length; i++)
+                SB.Append(Content[rnd.Next(Content.Length)]);
+            if (endzeichen != "")
+            {
+                SB.Append(endzeichen);
+            }
+            return SB.ToString();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Mk0.Tools.Randomization
+(C) 2019 mk0.at
+
+Allows you to generate a random string with a length of your choice.
+You can add an optional prefix and an optional appendix to the random string.
+
+You can use the following methods:
+
++ NumbersAndDigits (1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw)
++ Digits (ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw)
++ Numbers (1234567890)
+
+Example usage:
+
+Randomize.NumbersAndDigits(5, "_")
+will return f.e. "_j7Ga9"
+
+Randomize.Numbers(2, "*", "#")
+will return f.w. "*59#"
+
+Randomize.Digits(3)
+will return f.w. "fOu"
